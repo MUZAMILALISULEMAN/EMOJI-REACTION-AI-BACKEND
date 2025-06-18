0cc1077a64c3243249feee7f6106dcf43421966d
Muzamil Suleman <muzamil.ali1099@gmail.com>
2025-06-19 01:23:07 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["https://muzamilalisuleman.github.io/EMOJI-REACTION-AI-INTEGRATED/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
